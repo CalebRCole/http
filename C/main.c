71870c0fca9b698d6ff4ae8c1adc666947c8ef94
Caleb Cole <calebrylancole@gmail.com>
2026-03-11 00:55:05 -0400

    feat: Added socket creation
    Socket type is a IPv6 TCP stream.

--- C/main.c
+++ C/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main() {
-  printf("Hello World");
-  return 0;
+  int sockfd = socket(PF_INET6, SOCK_STREAM, IPPROTO_TCP);
+  if (sockfd == -1) {
+    fprintf(stderr, "Failed to create socket!\n");
+    return EXIT_FAILURE;
+  }
 }
