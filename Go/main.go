230b580c7515813978b1ee0188f9ab44f77d100a
Caleb Cole <calebrylancole@gmail.com>
2026-03-20 07:36:04 -0400

    feat: Working Go server
    Significantly less code than C/C++.

--- Go/main.go
+++ Go/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-     fmt.Println("Hello, World!")
+	var root string
+
+	if len(os.Args) > 1 {
+		root = os.Args[1]
+	} else {
+		root = "."
+	}
+
+	fs := http.FileServer(http.Dir(root))
+
+	http.Handle("/", fs)
+
+	log.Println("Listening on :8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
